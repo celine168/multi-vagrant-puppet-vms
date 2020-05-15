8e113cbc00a6bd619c40f2cbb9e3dcc969a18f6d
Celine L <celine168@gmail.com>
2020-05-15 13:33:04 -0700

    Add updated files for my second attempted setup

--- bootstrap-master.sh
+++ bootstrap-master.sh
@@ -15,9 +15,9 @@ else
     # Configure /etc/hosts file
     echo "" | sudo tee --append /etc/hosts 2> /dev/null && \
     echo "# Host config for Puppet Master and Agent Nodes" | sudo tee --append /etc/hosts 2> /dev/null && \
-    echo "192.168.32.5    puppet.example.com  puppet" | sudo tee --append /etc/hosts 2> /dev/null && \
-    echo "192.168.32.10   node01.example.com  node01" | sudo tee --append /etc/hosts 2> /dev/null && \
-    echo "192.168.32.20   node02.example.com  node02" | sudo tee --append /etc/hosts 2> /dev/null
+    echo "192.168.52.5    puppet2.example.com  puppet" | sudo tee --append /etc/hosts 2> /dev/null && \
+    echo "192.168.52.10   node01b.example.com  node01" | sudo tee --append /etc/hosts 2> /dev/null && \
+    echo "192.168.52.20   node02b.example.com  node02" | sudo tee --append /etc/hosts 2> /dev/null
 
     # Add optional alternate DNS names to /etc/puppet/puppet.conf
     sudo sed -i 's/.*\[main\].*/&\ndns_alt_names = puppet,puppet.example.com/' /etc/puppet/puppet.conf
